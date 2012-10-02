662802457dbe5c018359bfc1834172b6307abfad
Leonardo Ferreyra <leferreyra@gmail.com>
2012-10-02 14:49:01 -0300

    Agregado local settings.py file to gitignore

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ w3bex-website
 =============
 
 Sitio oficial de w3bex.com. Empresa de desarrollo de software Argentina.
+
--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,7 @@ env.hosts = ['root@w3bex.com:4190']
 remote_path = "/home/w3bex/w3bex"
 
 def deploy():
-    local("git add -p && git commit");
+    local("git -a commit");
     local("git push origin master")
     with cd(remote_path):
         run("git pull origin master")
