f105528af9d61b14eec5f4b56c0fa3f502013a95
Leonardo Ferreyra <leferreyra@gmail.com>
2012-10-02 13:53:22 -0300

    Modificado el fabfile para arreglar unos errores

--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,4 @@
+from __future__ import with_statement
 from fabric.api import *
 
 env.hosts = ['root@w3bex.com:4190']
@@ -6,6 +7,6 @@ remote_path = "/home/w3bex/w3bex"
 def deploy():
     local("git add -p && git commit");
     local("git push origin master")
-    cd(remote_path)
-    run("git pull origin master")
+    with cd(remote_path):
+        run("git pull origin master")
     
